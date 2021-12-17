2f7eff8ffad31f29c9463b7a2ecb60671a5b2d7d
ViRybalkin <kinjeks@gmail.com>
2021-12-17 10:28:07 +0300

    Добавил фильтрацию и убрал проблему с дубликацией задач

--- src/app.ts
+++ src/app.ts
@@ -101,7 +101,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`
 
     projectState.addListeners((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(prj =>{
+        if(this.type === 'active') {
+          return prj.status === ProjectStatus.active
+        }
+          return prj.status === ProjectStatus.finished
+      })
+      this.assignedProjects = relevantProjects;
       this.renderProjects()
     })
     this.attach()
@@ -110,6 +116,7 @@ class ProjectList {
 
   private renderProjects(){
     const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
